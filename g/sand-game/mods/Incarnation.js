91b48c42d0a6e5c528121e0b4be288efb64c4674
feeshmaster <125420779+feeshmaster@users.noreply.github.com>
2023-11-16 09:45:33 -0600

    Create Incarnation.js

---
+++ g/sand-game/mods/Incarnation.js
@@ -0,0 +1,8 @@
+elements.incarnation = {
+    color: "#ff5f5f",
+    tool: function(pixel) {
+        pixel.temp += 9837429837948374983247937439284739247392879898732497943298;
+		pixelTempCheck(pixel)
+    },
+    category: "tools",
+};
